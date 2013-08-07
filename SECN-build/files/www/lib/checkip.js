f33ccd9c8e7c1cf20f88a8111ef69ace08498225
Terry Gillett <tgillett@gmail.com>
2013-08-07 11:34:32 +1000

    Updated field validation
    Updated to limit use of special characters in text fields such as passwords, ssid etc.
    Accepted characters are a-z, A-Z, 0-9, *_-. only.
    
    Added validation to fields where it was not previously applied.
    
    All fields are now validated.

--- SECN-build/files/www/lib/checkip.js
+++ SECN-build/files/www/lib/checkip.js
@@ -20,6 +20,10 @@ $(document).ready( function() {
 		var ip = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 		return value.match(ip);
 	}, 'Invalid IP address');
+	$.validator.addMethod('PassChecker', function(value) {
+	        var pass ="^[a-zA-Z0-9_*.\-]*$"; 
+	        return value.match(pass);
+	}, 'Sorry, special characters not permitted');
 	$.validator.addMethod('HexChecker', function(value) {
 		var hex = "^([0-9a-fA-F]{2}([:-]|$)){6}$|([0-9a-fA-F]{4}([.]|$)){3}$";
 		return value.match(hex);
@@ -35,50 +39,25 @@ $(document).ready( function() {
 			required: true,
 			IP4Checker: true
 			},
-			BR_DNS: {
-			IP4Checker: true
-			},
-			BR_NETMASK: {
-			IP4Checker: true
-			},
-			ATH0_IPADDR: {
-			IP4Checker: true
-			},
-			ATH0_GATEWAY: {
-			IP4Checker: true
-			},
-			ATH0_DNS: {
-			IP4Checker: true
-			},
-			ATH0_NETMASK: {
-			IP4Checker: true
-			},
-			EXTERNIP: {
-			IP4Checker: true
-			},
-			STARTIP: {
-			IP4Checker: true
-			},
-			ENDIP: {
-			IP4Checker: true
-			},
-			OPTION_ROUTER: {
-			IP4Checker: true
-			},
-			ATH0_TXPOWER: {
-			range: [0, 20]
-			},
-			ENCRYPTION: {
-			required: true
-			},
-			ATH0_BSSID: {
-			HexChecker: true
+			SSID: {
+			PassChecker: true
 			},
 			PASSPHRASE: {
-			minlength: 8
+			minlength: 8,
+			PassChecker: true
+			},
+			HOST: {
+			PassChecker: true
+			},
+			USER: {
+			PassChecker: true
+			},
+			SECRET: {
+			PassChecker: true
 			},
 			PASSWORD1: {
-			minlength: 3
+			minlength: 3,
+			PassChecker: true
 			},
 			PASSWORD2: {
 			minlength: 3,
@@ -130,31 +109,48 @@ $(document).ready( function() {
 			OPTION_ROUTER: {
 			IP4Checker: true
 			},
+			DOMAIN: {
+			PassChecker: true
+			},
+			MAXLEASES: {
+			range: [0, 254]
+			},
+			LEASETERM: {
+			range: [0, 65536]
+			},
 			ATH0_TXPOWER: {
 			range: [0, 20]
-			},
-			ENCRYPTION: {
-			required: true
 			},
 			ATH0_BSSID: {
 			HexChecker: true
 			},
+			ATH0_SSID: {
+			PassChecker: true
+			},
+			SSID: {
+			PassChecker: true
+			},
 			PASSPHRASE: {
-			minlength: 8
+			minlength: 8,
+			PassChecker: true
 			},
-			PASSWORD1: {
-			minlength: 3
+			HOST: {
+			PassChecker: true
 			},
-			PASSWORD2: {
-			minlength: 3,
-			equalTo: "#PASSWORD1"
-			}
+			REGHOST: {
+			PassChecker: true
+			},
+			USER: {
+			PassChecker: true
+			},
+			SECRET: {
+			PassChecker: true
+			},
 		},
 		success: function(label) { 
 			label.html("").addClass("checked");
 		}
 	});
 });
-
 
 
