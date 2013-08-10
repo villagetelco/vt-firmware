2fcf81432827db91ba8ada0927f126d5c2cb3fc3
tgillett <tgillett@gmail.com>
2013-08-10 19:48:40 +1000

    Initial addition of MP-02 code

--- Build-MP-02.sh
+++ Build-MP-02.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Build script for UBNT devices
+# Build script for MP-02 devices
 
 echo ""
 
@@ -18,13 +18,8 @@ fi
 echo "Start build process"
 
 # Set up version strings
-<<<<<<< HEAD
-VER="Version 2.0 Beta 1f"
-DIRVER="Beta-1f"
-=======
-VER="SECN Version 2.0 xxxx"
-DIRVER="xxxx"
->>>>>>> Fix-1f
+VER="SECN Version 2.0 RC1c"
+DIRVER="RC1c"
 
 ###########################
 
@@ -36,7 +31,7 @@ cp -rp ~/Git/vt-firmware/SECN-build/ .
 
 echo "Set up new directory name with date and version"
 DATE=`date +%Y-%m-%d-%H:%M`
-DIR=$DATE"-UBNT-"$DIRVER
+DIR=$DATE"-MP02-"$DIRVER
 
 ###########################
 
@@ -51,27 +46,33 @@ touch ./bin/ar71xx/builds/build-$DIR/md5sums
 
 echo '----------------------------'
 
-echo "Set up .config for UBNT"
+echo "Set up .config for MP-02"
 rm ./.config
-cp ./SECN-build/UBNT/.config  ./.config
+cp ./SECN-build/MP-02/.config  ./.config
 echo " Run defconfig"
 make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/UBNT/.config 
 
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
+## Use for first build on a new revision to update .config file
+#cp ./.config ./SECN-build/MP-02/.config 
+
+## Get target device from .config file
+##TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
+# Set target as MP-02
+TARGET="MP-02"
 
 echo "Check .config version"
 cat ./.config | grep "OpenWrt version"
 echo "Target:  " $TARGET
 
-echo "Set up files for Ubiquity AR71xx"
-DEVICE="Ubiquity AR71xx"
+echo "Set up files for MP-02 "
+rm -r ./files
 
-rm -r ./files/*
-cp -r ./SECN-build/files       .        ; echo "Copy generic files"
-cp -r ./SECN-build/UBNT/files  .        ; echo "Overlay device specific files"
+echo "Copy generic files"
+cp -r ./SECN-build/files       .  
+
+echo "Overlay device specific files"
+cp -r ./SECN-build/MP-02/files .    
+
 ./FactoryRestore.sh			; echo "Build Factory Restore tar file"
 
 echo "Check files "
@@ -80,33 +81,37 @@ echo ""
 
 # Set up version file
 echo "Version: "  $VER $TARGET
-echo $VER  " " $TARGET           > ./files/etc/secn_version
+echo $VER  $TARGET               > ./files/etc/secn_version
 echo "Date stamp the version file: " $DATE
 echo "Build date " $DATE         >> ./files/etc/secn_version
 echo " "                         >> ./files/etc/secn_version
-
+ 
 echo "Check banner version"
 cat ./files/etc/secn_version | grep "Version"
 echo ""
 
-echo "Run make for UBNT"
-make
+echo "Run make for MP-02"
+make -j8
 
-echo  "Move files to build folder"  ##############
+echo  "Move files to build folder"
 mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-cat ./bin/ar71xx/md5sums | grep "lzma"                   >> ./bin/ar71xx/builds/build-$DIR/md5sums
+mv ./bin/ar71xx/*dragino2*.bin ./bin/ar71xx/builds/build-$DIR
+mv ./bin/ar71xx/*u-boot.bin ./bin/ar71xx/builds/build-$DIR
 
 echo "Clean up unused files"
 rm ./bin/ar71xx/openwrt-*
-
+echo "Update md5sums"
+cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
+cat ./bin/ar71xx/md5sums | grep "dragino2" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
+cat ./bin/ar71xx/md5sums | grep "u-boot"   | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
 echo ""
-echo "End Ubiquity AR71xx build"
+echo "End MP-02 build"
 echo ""
 
-#exit
+##################
+
+echo '----------------------------'
+
+echo " Build script complete"; echo " "
 
 
