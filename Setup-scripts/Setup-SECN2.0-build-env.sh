60ff2a5a0e9ef388e0af4e1680270e1f86810e9a
tgillett <tgillett@gmail.com>
2013-09-09 21:44:13 +1000

    Updated Setup script

--- Setup-scripts/Setup-SECN2.0-build-env.sh
+++ Setup-scripts/Setup-SECN2.0-build-env.sh
@@ -20,10 +20,12 @@ fi
 
 REPO_PATH=$(pwd)
 OPENWRT_PATH=$1
+
+mkdir $OPENWRT_PATH
+
 echo " "
-echo "*** Checkout the Dragino2 board repo to the current directory"
-sleep 5
-svn co http://svn.dragino.com/dragino2/    $REPO_PATH/dragino2
+echo " Get SECN 2 packages from GitHub repo"
+git clone https://github.com/villagetelco/vt-secn2-packages $OPENWRT_PATH/vt-secn2-packages
 
 echo " "
 echo "*** Checkout the OpenWRT build environment to the path specified on the command line"
@@ -32,34 +34,45 @@ mkdir -p $OPENWRT_PATH
 svn co svn://svn.openwrt.org/openwrt/trunk/@r33887 $OPENWRT_PATH
 
 echo " "
-echo "*** Backup original feeds files"
+echo "*** Backup original feeds files if they exist"
 sleep 5
 mv $OPENWRT_PATH/feeds.conf.default $OPENWRT_PATH/feeds.conf.default.bak
 mv $OPENWRT_PATH/feeds.conf $OPENWRT_PATH/feeds.conf.bak
 
 echo " "
-echo "Create new 'feeds.conf.default' file with lines for OpenWrt feed and Dragino feed"
-
-echo "src-svn packages svn://svn.openwrt.org/openwrt/packages@33887"        > $OPENWRT_PATH/feeds.conf.default
-echo "src-link dragino2 $REPO_PATH/dragino2/package"                        >> $OPENWRT_PATH/feeds.conf.default
-echo "src-git telephony http://feeds.openwrt.nanl.de/openwrt/telephony.git" >> $OPENWRT_PATH/feeds.conf.default
-echo "src-git alfred git://git.open-mesh.org/openwrt-feed-alfred.git"       >> $OPENWRT_PATH/feeds.conf.default
-
-# Not reqd if batman-adv included in Dragino repo
-#echo "src-git routing git://github.com/openwrt-routing/packages.git"      >> $OPENWRT_PATH/feeds.conf.default
-
+echo "***Create new 'feeds.conf.default' file for OpenWrt feed"
+echo "src-svn packages svn://svn.openwrt.org/openwrt/packages@33887" > $OPENWRT_PATH/feeds.conf.default
 echo " "
-echo "***Update the feeds"
+echo "***Update the OpenWrt feed"
 sleep 5
 $OPENWRT_PATH/scripts/feeds update
 
 echo " "
-echo "***Copy Dragino2 Platform info"
-sleep 5
-rsync -avC $REPO_PATH/dragino2/platform/target/ $OPENWRT_PATH/target/
+echo "***Create new 'feeds.conf.default' file for VT feeds"
+# This line is just for the record
+echo "#src-svn packages svn://svn.openwrt.org/openwrt/packages@33887"       > $OPENWRT_PATH/feeds.conf.default
+# These are the active lines
+echo "src-link dragino2   $OPENWRT_PATH/vt-secn2-packages/packages"         >> $OPENWRT_PATH/feeds.conf.default
+echo "src-link alfred     $OPENWRT_PATH/vt-secn2-packages/packages"         >> $OPENWRT_PATH/feeds.conf.default
+echo "src-link batman-adv $OPENWRT_PATH/vt-secn2-packages/packages/routing" >> $OPENWRT_PATH/feeds.conf.default
+
+# Original feeds for the record
+#echo "src-git routing git://github.com/openwrt-routing/packages.git" >> $OPENWRT_PATH/feeds.conf.default
+#echo "src-git telephony http://feeds.openwrt.nanl.de/openwrt/telephony.git" >> $OPENWRT_PATH/feeds.conf.default
+#echo "src-git alfred git://git.open-mesh.org/openwrt-feed-alfred.git" >> $OPENWRT_PATH/feeds.conf.default
 
 echo " "
-echo "***Install Dragino packages"
+echo "***Update the package feeds"
+sleep 5
+$OPENWRT_PATH/scripts/feeds update
+
+echo " "
+echo "***Copy MP02 Platform info"
+sleep 5
+rsync -avC $OPENWRT_PATH/vt-secn2-packages/platform/target/ $OPENWRT_PATH/target/
+
+echo " "
+echo "***Install MP02 hardware packages"
 sleep 5
 $OPENWRT_PATH/scripts/feeds install -a -p dragino2
 
@@ -78,7 +91,7 @@ $OPENWRT_PATH/scripts/feeds install alfred
 #rm tmp directory
 rm -rf $OPENWRT_PATH/tmp/
 
-# Change to new build directory
+# Change to build directory
 cd $OPENWRT_PATH
 
 echo " "
@@ -86,4 +99,3 @@ echo "*** Run < make menuconfig > to check and save configuration "
 echo " "
 
 # End of script
-
