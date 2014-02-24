42c8fc514bec7a72e2b1b0d390aedb3244bf65b3
tgillett <tgillett@gmail.com>
2014-02-24 20:35:14 +1000

    Updated TP Build script

--- Build-scripts/Build-TP.sh
+++ Build-scripts/Build-TP.sh
@@ -1,20 +1,20 @@
-#! /bin/sh
+#! /bin/bash
 
 # Build script for TP Link devices
-
+ 
 echo ""
 
 # Check to see if setup has already run
 if [ ! -f ./already_configured ]; then 
   # make sure it only executes once
   touch ./already_configured  
-  echo " Make builds directory"
+  echo "Make builds directory"
   mkdir ./bin/
   mkdir ./bin/ar71xx/
   mkdir ./bin/ar71xx/builds
   mkdir ./bin/atheros/
   mkdir ./bin/atheros/builds
-  echo " Initial set up completed. Continuing with build"
+  echo "Initial set up completed. Continuing with build"
   echo ""
 else
   echo "Build environment is configured. Continuing with build"
@@ -26,8 +26,8 @@ fi
 
 echo "Start build process"
 
-# Set up version strings
-DIRVER="RC3d"
+echo "Set up version strings"
+DIRVER="RC4a"
 VER="SECN Version 2.0 "$DIRVER
 
 ###########################
@@ -52,38 +52,43 @@ mkdir ./bin/ar71xx/builds/build-$DIR
 # Create md5sums file
 touch ./bin/ar71xx/builds/build-$DIR/md5sums
 
-###########################
+##########################
 
-echo '----------------------------'
+# Build function
 
-echo "Set up .config for TL-WR842"
+function build_tp() {
+echo "Set up .config for "$1
 rm ./.config
-cp ./SECN-build/WR842/.config  ./.config
-echo " Run defconfig"
+cp ./SECN-build/$1/config-$1  ./.config
+echo "Run defconfig"
 make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/WR842/.config 
 
 # Get target device from .config file
 TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
+#TARGET=$1
 
 echo "Check .config version"
 cat ./.config | grep "OpenWrt version"
 echo "Target:  " $TARGET
+echo ""
 
-echo "Set up files for TL-WR842 "
+echo "Set up files for "$1
+echo "Remove files directory"
 rm -r ./files/*
-cp -r ./SECN-build/files       .        ; echo "Copy generic files"
-cp -r ./SECN-build/WR842/files .        ; echo "Overlay device specific files"
+echo "Copy generic files"
+cp -r ./SECN-build/files       .        ; 
+echo "Overlay device specific files"
+cp -r ./SECN-build/$1/files .        ; 
+echo ""
 
 echo "Build Factory Restore tar file"
 ./FactoryRestore.sh	 
-
-echo "Check files "
-ls -al ./files   
 echo ""
 
-# Set up version file
+echo "Check files directory"
+ls -al ./files  
+
+echo ""
 echo "Version: "  $VER $TARGET
 echo $VER  $TARGET               > ./files/etc/secn_version
 echo "Date stamp the version file: " $DATE
@@ -94,419 +99,50 @@ echo "Check banner version"
 cat ./files/etc/secn_version | grep "Version"
 echo ""
 
-echo "Run make for WR842"
+echo "Run make for "$1
 make
+echo ""
 
 echo  "Move files to build folder"
 mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
 mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
+echo ""
 
 echo "Clean up unused files"
 rm ./bin/ar71xx/openwrt-*
+echo ""
+
 echo "Update md5sums"
 cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
 echo ""
-echo "End WR842 build"
+
+echo "End " $1 " build"
 echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
 echo '----------------------------'
+}
 
-echo "Set up .config for TL-MR3020"
-rm ./.config
-cp ./SECN-build/MR3020/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/MR3020/.config 
-
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-MR3020 "
-rm -r ./files/*
-cp -r ./SECN-build/files       .        ; echo "Copy generic files"
-cp -r ./SECN-build/MR3020/files .       ; echo "Overlay device specific files"
-./FactoryRestore.sh											; echo "Build Factory Restore tar file"
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for MR3020"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End MR3020 build"
-echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
-echo '----------------------------'
-
-echo "Set up .config for TL-WR703"
-rm ./.config
-cp ./SECN-build/WR703/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/WR703/.config 
-
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-WR703 "
-rm -r ./files/*
-cp -r ./SECN-build/files       .        ; echo "Copy generic files"
-cp -r ./SECN-build/WR703/files .        ; echo "Overlay device specific files"
-./FactoryRestore.sh			; echo "Build Factory Restore tar file"
-
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for WR703"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*wr703*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*wr703*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End WR703 build"
-echo ""
-
-###############
-#exit  #  Uncomment to end the build process here
+############################
 
 
 echo '----------------------------'
-
-echo "Set up .config for TL-WDR4300, 4310, 3600"
-rm ./.config
-cp ./SECN-build/WDR4300/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/WDR4300/.config 
-
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-WDR4300 "
-rm -r ./files/*
-cp -r ./SECN-build/files          .       ; echo "Copy generic files"
-cp -r ./SECN-build/WDR4300/files  .       ; echo "Overlay device specific files"
-./FactoryRestore.sh			  ; echo "Build Factory Restore tar file"
-
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for WDR4300"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*wdr4300*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*wdr4300*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*wdr4310*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*wdr4310*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*wdr3600*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*wdr3600*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End WDR4300 build"
-echo ""
-
-#################
-#exit  #  Uncomment to end the build process here
-
-
+echo " "
+echo "Start Device builds"
+echo " "
 echo '----------------------------'
 
-echo "Set up .config for TL-MR11U"
-rm ./.config
-cp ./SECN-build/MR11U/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/MR11U/.config 
+build_tp WR842
+build_tp WDR4300
+build_tp MR3020
+build_tp WR703
+build_tp MR3420
+build_tp WR741
+build_tp WR841
+build_tp MR11U
 
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-MR11U"
-rm -r ./files/*
-cp -r ./SECN-build/files          .       ; echo "Copy generic files"
-cp -r ./SECN-build/MR11U/files    .       ; echo "Overlay device specific files"
-./FactoryRestore.sh			  ; echo "Build Factory Restore tar file"
-
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for MR11U"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End MR11U build"
-echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
+echo " "
+echo "Build script complete"
+echo " "
 echo '----------------------------'
 
-echo "Set up .config for TL-MR3420"
-rm ./.config
-cp ./SECN-build/MR3420/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/MR3420/.config 
-
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-MR3420"
-rm -r ./files/*
-cp -r ./SECN-build/files          .       ; echo "Copy generic files"
-cp -r ./SECN-build/MR3420/files   .       ; echo "Overlay device specific files"
-./FactoryRestore.sh			  ; echo "Build Factory Restore tar file"
-
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for MR3420"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End MR3420 build"
-echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
-echo '----------------------------'
-
-echo "Set up .config for TL-WR841"
-rm ./.config
-cp ./SECN-build/WR841/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/WR841/.config 
-
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-WR841"
-rm -r ./files/*
-cp -r ./SECN-build/files          .       ; echo "Copy generic files"
-cp -r ./SECN-build/WR841/files   .       ; echo "Overlay device specific files"
-./FactoryRestore.sh			  ; echo "Build Factory Restore tar file"
-
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for WR841"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End WR841 build"
-echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
-echo '----------------------------'
-
-echo "Set up .config for TL-WR741"
-rm ./.config
-cp ./SECN-build/WR741/.config  ./.config
-echo " Run defconfig"
-make defconfig > /dev/null
-## Use for first build on a new revision to update .config file
-cp ./.config ./SECN-build/WR741/.config 
-
-# Get target device from .config file
-TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-
-echo "Check .config version"
-cat ./.config | grep "OpenWrt version"
-echo "Target:  " $TARGET
-
-echo "Set up files for TL-WR741"
-rm -r ./files/*
-cp -r ./SECN-build/files          .       ; echo "Copy generic files"
-cp -r ./SECN-build/WR741/files   .       ; echo "Overlay device specific files"
-./FactoryRestore.sh			  ; echo "Build Factory Restore tar file"
-
-echo "Check files "
-ls -al ./files   
-echo ""
-
-# Set up version file
-echo "Version: "  $VER $TARGET
-echo $VER  $TARGET               > ./files/etc/secn_version
-echo "Date stamp the version file: " $DATE
-echo "Build date " $DATE         >> ./files/etc/secn_version
-echo " "                         >> ./files/etc/secn_version
- 
-echo "Check banner version"
-cat ./files/etc/secn_version | grep "Version"
-echo ""
-
-echo "Run make for WR741"
-make
-
-echo  "Move files to build folder"
-mv ./bin/ar71xx/*-squash*sysupgrade.bin ./bin/ar71xx/builds/build-$DIR
-mv ./bin/ar71xx/*-squash*factory.bin    ./bin/ar71xx/builds/build-$DIR
-echo "Clean up unused files"
-rm ./bin/ar71xx/openwrt-*
-echo "Update md5sums"
-cat ./bin/ar71xx/md5sums | grep "squashfs" | grep ".bin" >> ./bin/ar71xx/builds/build-$DIR/md5sums
-echo ""
-echo "End WR741 build"
-echo ""
-
-##################
-#exit  #  Uncomment to end the build process here
-
-echo '----------------------------'
-
-
-
-echo " Build script complete"; echo " "
-
+exit
 
