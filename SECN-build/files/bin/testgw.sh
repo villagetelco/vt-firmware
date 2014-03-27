27f9b7be414b656a00d5cd3598a110948ba369b9
tgillett <tgillett@gmail.com>
2014-03-27 10:04:52 +1000

    Changed screen labels for LAN and Mesh Gateways, changed Internet Access Test button on Basic page

--- SECN-build/files/bin/testgw.sh
+++ SECN-build/files/bin/testgw.sh
@@ -1,33 +1,51 @@
 #!/bin/sh
 
+# Test for Internet access 
+
+ping -w 1 '8.8.8.8' > /dev/null; RETVAL=$?  
+
+if [ $RETVAL -eq 0 ]; then
+	INTTXT="Internet access available.   "
+else
+	INTTXT="No Internet access.   "
+fi
+
+
 # Find network details with DHCP Discover request
 
 # Set up a temp iface
 ifconfig br-lan:99 1.1.1.1
 
-# Look for a DHCP server and call the script to get the env variables
+# Look for a DHCP server and call the script to get and save the env variables
 udhcpc -n -i br-lan:99 -s /bin/savedhcp.sh > /dev/null
 
 # Remove the temp iface
 ifconfig br-lan:99 down
 
-# Get the saved gateway address
+# Get the saved LAN Gateway and DNS addresses
 GW=`cat /tmp/gateway.txt | grep .`
 DNS=`cat /tmp/dns.txt | grep .`
 
-# If no Gateway found set to 0.0.0.0 and prepare status message text
+# For testing
+#GW="192.168.1.254"
+#DNS="192.168.1.254"
+
+# If no Gateway found, prepare status message text and set to 0.0.0.0
 if [ $GW ]; then
   if [ $DNS ]; then
-    echo "   Gateway found at " $GW "...   DNS Server found at " $DNS > /tmp/gatewaystatus.txt
+    GWTXT=" LAN DHCP Server - LAN Gateway: "$GW" DNS: "$DNS
   else
-    echo "   Gateway found at " $GW "   No DNS Server found" > /tmp/gatewaystatus.txt
+    GWTXT=" LAN DHCP Server - LAN Gateway: "$GW 
   fi
 else
   GW="0.0.0.0"
-  echo No Gateway Found > /tmp/gatewaystatus.txt
+  #GWTXT=" No LAN Gateway found"
 fi
 
-# Output message if script run manually
+# Generate message string
+echo $INTTXT $GWTXT > /tmp/gatewaystatus.txt
+
+# Output message if script run manually for testing
 #cat /tmp/gatewaystatus.txt
 
 # Save the Gateway address if reqd
