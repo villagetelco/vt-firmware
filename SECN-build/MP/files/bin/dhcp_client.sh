65fe401496880e43f004b9e4aa505d5423de6601
tgillett <tgillett@gmail.com>
2015-01-28 08:12:59 +1000

    Removed redundant directories

--- SECN-build/MP/files/bin/dhcp_client.sh
+++
@@ -1,9 +0,0 @@
-echo "Starting dhcp client in 10 seconds. This will not change the device configuration."
-echo "If you are disconnected, statically configure your workstation to use"
-echo "IP 172.31.255.253 NETMASK 255.255.255.252 and connect to 172.31.255.254"
-echo "- thats the fallback IP setting of the Mesh-Potato." 
-
-udhcpc -i eth0 
-
-
-
