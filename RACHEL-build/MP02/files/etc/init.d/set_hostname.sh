44f4dde26811b8b22721939b1dc4aed44e308841
tgillett <tgillett@gmail.com>
2015-02-19 09:35:13 +1000

    Initial commit for VT RACHEL code

---
+++ RACHEL-build/MP02/files/etc/init.d/set_hostname.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOST="RACHEL"
+
+# Set the hostname
+uci set system.@system[0].hostname=$HOST
+uci commit system
+
+# Set the hostname as the Common Name in the SSL certificate for the web server.
+uci set uhttpd.px5g.commonname=$HOST
+uci commit uhttpd
+
+# Set the system hostname
+echo $(uci get system.@system[0].hostname) > /proc/sys/kernel/hostname
+
+
