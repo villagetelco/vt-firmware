2b654ff82ce45c5aa9891871658e831d921f1a51
tgillett <tgillett@gmail.com>
2015-06-22 11:38:36 +1000

    Updated build scripts for md5sums file names

--- Build-scripts/Build-MP01.sh
+++ Build-scripts/Build-MP01.sh
@@ -78,8 +78,8 @@ echo "Set up new build directory  $BINDIR/builds/build-"$DIR
 mkdir $BINDIR/builds/build-$DIR
 
 # Create md5sums files
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums-$VER
+echo $DIR > $BINDIR/builds/build-$DIR/md5sums.txt
+echo $DIR > $BINDIR/builds/build-$DIR/md5sums-$VER.txt
 
 ##########################
 
@@ -182,12 +182,12 @@ make
 echo ""
 
 echo "Update original md5sums file"
-cat $BINDIR/md5sums | grep "openwrt"  >> $BINDIR/builds/build-$DIR/md5sums
+cat $BINDIR/md5sums | grep "openwrt"  >> $BINDIR/builds/build-$DIR/md5sums.txt
 echo ""
 
 echo "Update new md5sums file"
-md5sum $BINDIR/*root.squashfs >> $BINDIR/builds/build-$DIR/md5sums-$VER
-md5sum $BINDIR/*.lzma         >> $BINDIR/builds/build-$DIR/md5sums-$VER
+md5sum $BINDIR/*root.squashfs >> $BINDIR/builds/build-$DIR/md5sums-$VER.txt
+md5sum $BINDIR/*.lzma         >> $BINDIR/builds/build-$DIR/md5sums-$VER.txt
 
 echo  "Move files to build folder"
 mv $BINDIR/*root.squashfs  $BINDIR/builds/build-$DIR
--- Build-scripts/Build-MP02-CR.sh
+++ Build-scripts/Build-MP02-CR.sh
@@ -82,8 +82,8 @@ echo "Set up new build directory  $BINDIR/builds/build-"$DIR
 mkdir $BINDIR/builds/build-$DIR
 
 # Create md5sums files
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums-$VER
+echo $DIR > $BINDIR/builds/build-$DIR/md5sums.txt
+echo $DIR > $BINDIR/builds/build-$DIR/md5sums-$VER.txt
 
 ##########################
 
@@ -156,9 +156,9 @@ make -j8
 echo ""
 
 echo "Update original md5sums file"
-cat $BINDIR/md5sums | grep "squashfs.bin"   | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
-cat $BINDIR/md5sums | grep "kernel.bin"     | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
-cat $BINDIR/md5sums | grep "sysupgrade.bin" | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
+cat $BINDIR/md5sums | grep "squashfs.bin"   | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums.txt
+cat $BINDIR/md5sums | grep "kernel.bin"     | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums.txt
+cat $BINDIR/md5sums | grep "sysupgrade.bin" | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums.txt
 echo ""
 
 echo  "Rename files to add version info"
@@ -170,9 +170,9 @@ else
 fi
 
 echo "Update new md5sums file"
-md5sum $BINDIR/*-squash*sysupgrade.bin >> $BINDIR/builds/build-$DIR/md5sums-$VER
-md5sum $BINDIR/openwrt*kernel.bin >>     $BINDIR/builds/build-$DIR/md5sums-$VER
-md5sum $BINDIR/openwrt*squashfs.bin >>   $BINDIR/builds/build-$DIR/md5sums-$VER
+md5sum $BINDIR/*-squash*sysupgrade.bin >> $BINDIR/builds/build-$DIR/md5sums-$VER.txt
+md5sum $BINDIR/openwrt*kernel.bin >>     $BINDIR/builds/build-$DIR/md5sums-$VER.txt
+md5sum $BINDIR/openwrt*squashfs.bin >>   $BINDIR/builds/build-$DIR/md5sums-$VER.txt
 
 echo  "Move files to build folder"
 mv $BINDIR/openwrt*-squash*sysupgrade.bin $BINDIR/builds/build-$DIR
