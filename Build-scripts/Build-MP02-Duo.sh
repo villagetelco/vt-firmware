036767a6ea69b833464b7d072ab054b5e32cb6dc
tgillett <tgillett@gmail.com>
2015-09-02 12:46:05 +1000

    Updated build scripts for new version

--- Build-scripts/Build-MP02-Duo.sh
+++ Build-scripts/Build-MP02-Duo.sh
@@ -6,11 +6,16 @@
 
 # Select the repo to use
 REPO="vt-firmware"
+BRANCH="secn_3.0-Duo"
+
+echo "Set up version strings"
+DIRVER="Duo-GA01.2"
+VER="SECN-3.0-MP02-"$DIRVER
 
 
 echo "************************************"
 echo ""
-echo "Build script for MP-02 device"
+echo "Build script for MP02 Duo device"
 
 echo "Git directory: "$GITREPO
 echo "Repo: "$REPO
@@ -22,28 +27,37 @@ if [ ! -d $GITREPO"/"$REPO ]; then
 	exit
 fi
 
-echo "Check out the correct branch"
+
+echo "Check out the correct vt-firmware branch - $BRANCH"
+
 BUILD_DIR=$(pwd)
 cd $GITREPO"/"$REPO
-git checkout secn_3.0-Duo > /dev/null
+git checkout $BRANCH > /dev/null
+# Make sure checkout worked
+CHK_BR=`git branch | grep "*" | cut -d " " -f2`
+if [ $CHK_BR != $BRANCH ]; then
+	echo "Branch checkout failed"
+	echo "*****"
+	exit
+else
+	echo "Branch checkout successful"
+fi
 git branch | grep "*"
 cd $BUILD_DIR
 pwd
 
 ##############################
 
-
-
 # Check to see if setup has already run
 if [ ! -f ./already_configured ]; then 
   # make sure it only executes once
   touch ./already_configured  
   echo "Make builds directory"
-  mkdir ./bin/
-  mkdir ./bin/ar71xx/
-  mkdir ./bin/ar71xx/builds
-  mkdir ./bin/atheros/
-  mkdir ./bin/atheros/builds
+  mkdir ./Builds/
+  mkdir ./Builds/ar71xx/
+  mkdir ./Builds/ar71xx/builds
+  mkdir ./Builds/atheros/
+  mkdir ./Builds/atheros/builds
   echo "Initial set up completed. Continuing with build"
   echo ""
 else
@@ -56,15 +70,15 @@ fi
 
 echo "Start build process"
 
-echo "Set up version strings"
-DIRVER="BB-Alpha8-Duo"
-VER="SECN-3_0-"$DIRVER
+BINDIR="./bin/ar71xx"
+BUILDDIR="./Builds/ar71xx"
 
 ###########################
 echo "Copy files from Git repo into build folder"
 rm -rf ./SECN-build/
 cp -rp $GITREPO/$REPO/SECN-build/ .
 cp -fp $GITREPO/$REPO/Build-scripts/FactoryRestore.sh  .
+cp -fp $GITREPO/$REPO/Build-scripts/GetGitVersions.sh  .
 
 
 ###########################
@@ -82,14 +96,13 @@ DATE=`date +%Y-%m-%d-%H:%M`
 DIR=$DATE"-MP02-"$DIRVER
 
 ###########################
-BINDIR="./bin/ar71xx"
 # Set up build directory
-echo "Set up new build directory  $BINDIR/builds/build-"$DIR
-mkdir $BINDIR/builds/build-$DIR
+echo "Set up new build directory  $BUILDDIR/builds/build-"$DIR
+mkdir $BUILDDIR/builds/build-$DIR
 
 # Create md5sums files
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums-$VER
+echo $DIR > $BUILDDIR/builds/build-$DIR/md5sums.txt
+echo $DIR > $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
 
 ##########################
 
@@ -101,11 +114,11 @@ echo "Set up .config for "$1 $2
 rm ./.config
 
 if [ $2 ]; then
-	echo "Config file: config-BB-"$1-$2
-	cp ./SECN-build/$1/config-BB-$1-$2  ./.config
+	echo "Config file: config-"$1-$2
+	cp ./SECN-build/$1/config-$1-$2  ./.config
 else
-	echo "Config file: config-BB-"$1
-	cp ./SECN-build/$1/config-BB-$1  ./.config
+	echo "Config file: config-"$1
+	cp ./SECN-build/$1/config-$1  ./.config
 fi
 
 echo "Run defconfig"
@@ -113,11 +126,9 @@ make defconfig > /dev/null
 
 # Set target string
 TARGET=$1
-OPENWRTVER=`cat ./.config | grep "OpenWrt version" | cut -d : -f 2`
 
 echo "Check .config version"
 echo "Target:  " $TARGET
-#echo "OpenWRT: " $OPENWRTVER
 echo ""
 
 echo "Set up files for "$1 $2
@@ -143,7 +154,6 @@ echo "Version: " $VER $TARGET $2
 echo "Date stamp: " $DATE
 
 echo "Version:    " $VER $TARGET $2        > ./files/etc/secn_version
-#echo "OpenWRT:    " $OPENWRTVER           >> ./files/etc/secn_version
 echo "Build date: " $DATE                 >> ./files/etc/secn_version
 echo "GitHub:     " $REPO $REPOID         >> ./files/etc/secn_version
 echo " "                                  >> ./files/etc/secn_version
@@ -158,13 +168,14 @@ rm $BINDIR/openwrt-*
 echo ""
 
 echo "Run make for "$1 $2
-make
+make -j5
+#make -j1 V=s 2>&1 | tee ~/build.txt
 echo ""
 
 echo "Update original md5sums file"
-cat $BINDIR/md5sums | grep "squashfs.bin"   | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
-cat $BINDIR/md5sums | grep "kernel.bin"     | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
-cat $BINDIR/md5sums | grep "sysupgrade.bin" | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
+cat $BINDIR/md5sums | grep "squashfs.bin"   | grep ".bin" >> $BUILDDIR/builds/build-$DIR/md5sums.txt
+cat $BINDIR/md5sums | grep "kernel.bin"     | grep ".bin" >> $BUILDDIR/builds/build-$DIR/md5sums.txt
+cat $BINDIR/md5sums | grep "sysupgrade.bin" | grep ".bin" >> $BUILDDIR/builds/build-$DIR/md5sums.txt
 echo ""
 
 echo  "Rename files to add version info"
@@ -176,14 +187,14 @@ else
 fi
 
 echo "Update new md5sums file"
-md5sum $BINDIR/*-squash*sysupgrade.bin >> $BINDIR/builds/build-$DIR/md5sums-$VER
-md5sum $BINDIR/openwrt*kernel.bin >>     $BINDIR/builds/build-$DIR/md5sums-$VER
-md5sum $BINDIR/openwrt*squashfs.bin >>   $BINDIR/builds/build-$DIR/md5sums-$VER
+md5sum $BINDIR/*-squash*sysupgrade.bin >> $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
+md5sum $BINDIR/openwrt*kernel.bin      >> $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
+md5sum $BINDIR/openwrt*squashfs.bin    >> $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
 
 echo  "Move files to build folder"
-mv $BINDIR/openwrt*-squash*sysupgrade.bin $BINDIR/builds/build-$DIR
-mv $BINDIR/openwrt*kernel.bin     $BINDIR/builds/build-$DIR
-mv $BINDIR/openwrt*squashfs.bin   $BINDIR/builds/build-$DIR
+mv $BINDIR/openwrt*-squash*sysupgrade.bin $BUILDDIR/builds/build-$DIR
+mv $BINDIR/openwrt*kernel.bin             $BUILDDIR/builds/build-$DIR
+mv $BINDIR/openwrt*squashfs.bin           $BUILDDIR/builds/build-$DIR
 echo ""
 
 echo "Clean up unused files"
--- Build-scripts/Build-TP-Duo.sh
+++ Build-scripts/Build-TP-Duo.sh
@@ -6,6 +6,11 @@
 
 # Select the repo to use
 REPO="vt-firmware"
+BRANCH="secn_3.0-Duo"
+
+echo "Set up version strings"
+DIRVER="Duo-GA01.1"
+VER="SECN-3.0-TP-"$DIRVER
 
 
 echo "************************************"
@@ -22,28 +27,37 @@ if [ ! -d $GITREPO"/"$REPO ]; then
 	exit
 fi
 
-echo "Check out the correct branch"
+
+echo "Check out the correct vt-firmware branch - $BRANCH"
+
 BUILD_DIR=$(pwd)
 cd $GITREPO"/"$REPO
-git checkout secn_3.0-Duo > /dev/null
+git checkout $BRANCH > /dev/null
+# Make sure checkout worked
+CHK_BR=`git branch | grep "*" | cut -d " " -f2`
+if [ $CHK_BR != $BRANCH ]; then
+	echo "Branch checkout failed"
+	echo "*****"
+	exit
+else
+	echo "Branch checkout successful"
+fi
 git branch | grep "*"
 cd $BUILD_DIR
 pwd
 
 ##############################
 
-
-
 # Check to see if setup has already run
 if [ ! -f ./already_configured ]; then 
   # make sure it only executes once
   touch ./already_configured  
   echo "Make builds directory"
-  mkdir ./bin/
-  mkdir ./bin/ar71xx/
-  mkdir ./bin/ar71xx/builds
-  mkdir ./bin/atheros/
-  mkdir ./bin/atheros/builds
+  mkdir ./Builds/
+  mkdir ./Builds/ar71xx/
+  mkdir ./Builds/ar71xx/builds
+  mkdir ./Builds/atheros/
+  mkdir ./Builds/atheros/builds
   echo "Initial set up completed. Continuing with build"
   echo ""
 else
@@ -56,15 +70,15 @@ fi
 
 echo "Start build process"
 
-echo "Set up version strings"
-DIRVER="BB-Alpha8-Duo"
-VER="SECN-3_0-"$DIRVER
+BINDIR="./bin/ar71xx"
+BUILDDIR="./Builds/ar71xx"
 
 ###########################
 echo "Copy files from Git repo into build folder"
 rm -rf ./SECN-build/
 cp -rp $GITREPO/$REPO/SECN-build/ .
 cp -fp $GITREPO/$REPO/Build-scripts/FactoryRestore.sh  .
+cp -fp $GITREPO/$REPO/Build-scripts/GetGitVersions.sh  .
 
 
 ###########################
@@ -82,14 +96,13 @@ DATE=`date +%Y-%m-%d-%H:%M`
 DIR=$DATE"-TP-"$DIRVER
 
 ###########################
-BINDIR="./bin/ar71xx"
 # Set up build directory
-echo "Set up new build directory  $BINDIR/builds/build-"$DIR
-mkdir $BINDIR/builds/build-$DIR
+echo "Set up new build directory  $BUILDDIR/builds/build-"$DIR
+mkdir $BUILDDIR/builds/build-$DIR
 
 # Create md5sums files
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums
-echo $DIR > $BINDIR/builds/build-$DIR/md5sums-$VER
+echo $DIR > $BUILDDIR/builds/build-$DIR/md5sums.txt
+echo $DIR > $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
 
 ##########################
 
@@ -101,11 +114,11 @@ echo "Set up .config for "$1 $2
 rm ./.config
 
 if [ $2 ]; then
-	echo "Config file: config-BB-"$1-$2
-	cp ./SECN-build/$1/config-BB-$1-$2  ./.config
+	echo "Config file: config-"$1-$2
+	cp ./SECN-build/$1/config-$1-$2  ./.config
 else
-	echo "Config file: config-BB-"$1
-	cp ./SECN-build/$1/config-BB-$1  ./.config
+	echo "Config file: config-"$1
+	cp ./SECN-build/$1/config-$1  ./.config
 fi
 
 echo "Run defconfig"
@@ -113,11 +126,9 @@ make defconfig > /dev/null
 
 # Set up target display strings
 TARGET=`cat .config | grep "CONFIG_TARGET" | grep "=y" | grep "_generic_" | cut -d _ -f 5 | cut -d = -f 1 `
-#OPENWRTVER=`cat ./.config | grep "OpenWrt version" | cut -d : -f 2`
 
 echo "Check .config version"
 echo "Target:  " $TARGET
-#echo "OpenWRT: " $OPENWRTVER
 echo ""
 
 echo "Set up files for "$1 $2
@@ -143,7 +154,6 @@ echo "Version: " $VER $TARGET $2
 echo "Date stamp: " $DATE
 
 echo "Version:    " $VER $TARGET $2        > ./files/etc/secn_version
-#echo "OpenWRT:    " $OPENWRTVER           >> ./files/etc/secn_version
 echo "Build date: " $DATE                 >> ./files/etc/secn_version
 echo "GitHub:     " $REPO $REPOID         >> ./files/etc/secn_version
 echo " "                                  >> ./files/etc/secn_version
@@ -158,11 +168,12 @@ rm $BINDIR/openwrt-*
 echo ""
 
 echo "Run make for "$1 $2
-make
+make -j5
+#make -j1 V=s 2>&1 | tee ~/build.txt
 echo ""
 
 echo "Update original md5sums file"
-cat $BINDIR/md5sums | grep "squashfs" | grep ".bin" >> $BINDIR/builds/build-$DIR/md5sums
+cat $BINDIR/md5sums | grep "squashfs" | grep ".bin" >> $BUILDDIR/builds/build-$DIR/md5sums.txt
 echo ""
 
 echo  "Rename files to add version info"
@@ -174,11 +185,13 @@ else
 fi
 
 echo "Update new md5sums file"
-md5sum $BINDIR/*-squash*sysupgrade.bin >> $BINDIR/builds/build-$DIR/md5sums-$VER
-#md5sum $BINDIR/*-squash*factory.bin    >> $BINDIR/builds/build-$DIR/md5sums-$VER
+md5sum $BINDIR/*-squash*sysupgrade.bin >> $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
+#md5sum $BINDIR/*-squash*factory.bin    >> $BUILDDIR/builds/build-$DIR/md5sums-$VER.txt
+echo ""
 
 echo  "Move files to build folder"
-cp $BINDIR/openwrt*-squash*sysupgrade.bin $BINDIR/builds/build-$DIR
+mv $BINDIR/openwrt*-squash*sysupgrade.bin $BUILDDIR/builds/build-$DIR
+#mv $BINDIR/*-squash*factory.bin    $BUILDDIR/builds/build-$DIR
 echo ""
 
 echo "Clean up unused files"
@@ -202,6 +215,7 @@ echo '----------------------------'
 
 build_tp MR3020 Duo
 build_tp WR842 Duo
+exit
 build_tp WR703 Duo
 build_tp MR3040 Duo
 build_tp MR11U Duo
