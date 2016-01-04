f269ee39e23356d55c655ad337f8a571ef454c16
tgillett <tgillett@gmail.com>
2016-01-04 12:57:54 +1000

    Change cache stats

--- RACHEL-build/files/www/cgi-bin/config/conf-get-rachel.sh
+++ RACHEL-build/files/www/cgi-bin/config/conf-get-rachel.sh
@@ -14,8 +14,8 @@ UPTIME=`uptime`
 TZ=`cat /etc/TZ`
 
 # Cache
-CACHE_COUNT=`du -h /mnt/sda1/cache | grep -c "cache/" `
-CACHE_SIZE=`du -h /mnt/sda1/cache | grep -v "cache/" | cut -f 1`
+CACHE_COUNT=`ls -lh /mnt/sda1/ | grep cache | cut -d " " -f 2`
+CACHE_SPACE=`df -h | grep sda1 | sed -r "s'[[:blank:]]+','g" | cut -d ',' -f 4`
 
 # Get USB Modem status details
 USBMODEM=`/bin/usbmodem.sh`
