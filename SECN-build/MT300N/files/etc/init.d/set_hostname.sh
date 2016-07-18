b5c968febcbe1b16edfcf11f87d04a32af668bdf
tgillett <tgillett@gmail.com>
2016-07-18 12:22:56 +1000

    Added MT300N device

---
+++ SECN-build/MT300N/files/etc/init.d/set_hostname.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Set the host name prefix
+HOST="MT300A"
+
+# Build the hostname ($HOST-nnn) from the device IP address last octet
+OCTET_D=`uci get network.lan.ipaddr | cut -d = -f2 | cut -d . -f4`
+
+HOSTNAME="$HOST-$OCTET_D"
+
+# Set the hostname
+uci set system.@system[0].hostname=$HOSTNAME
+uci commit system
+
+# Set the hostname as the Common Name in the SSL certificate for the web server.
+uci set uhttpd.px5g.commonname=$HOSTNAME
+uci commit uhttpd
+
+# Set the system hostname
+echo $HOSTNAME > /proc/sys/kernel/hostname
+
+
