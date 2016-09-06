16be37ba4e88600b661a8b2459bf2d388a626e31
tgillett <tgillett@gmail.com>
2016-09-06 21:44:02 +1000

    Added reset function. Updated firmware restore to add config restore

--- SECN-build/files/bin/firmware-restore.sh
+++ SECN-build/files/bin/firmware-restore.sh
@@ -4,6 +4,11 @@
 # This script is called when button is pressed an released after 20 - 30 seconds
 # Author TLG
 
+# Restore configuration to factory defaults
+cd /etc
+tar -xzf conf-default.tar.gz >> /dev/null
+cd
+
 # Restore firmware
 rm -r /overlay/*  &&  reboot
 
