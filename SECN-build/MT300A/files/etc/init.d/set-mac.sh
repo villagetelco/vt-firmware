bcab74f6dfcf1fceac28823e0d921d34121b0cf6
tgillett <tgillett@gmail.com>
2017-01-28 17:27:08 +1000

    Updated set MAC script to fix MAC conflict on startup with LAN and bridge

--- SECN-build/MT300A/files/etc/init.d/set-mac.sh
+++ SECN-build/MT300A/files/etc/init.d/set-mac.sh
@@ -1,40 +1,44 @@
 #!/bin/sh
 
 # /etc/init.d/set-mac
-# This script looks for a MAC address conflict between AdHoc wifi interface and br-lan.
-# If there is a conflict, the MAC address of the AdHoc interface is changed in the third octet,
-# and is explictly set in the wireless config file.
-# This avoids batman-adv receiving packets on own MAC address
-# A MAC conflict will be resolved after first restart.
-
+# This script looks for a MAC address conflict between Mesh wifi interface and Ethernet LAN.
+# If there is a conflict, the MAC address of the LAN interface is changed in the third octet.
+# This avoids batman-adv receiving packets on own MAC address.
 
 WLAN="wlan0-1"
+LAN="eth0.1"
+BRIDGE="br-lan"
 
 # Get the current MAC addresses
 if [ -e /sys/class/net/$WLAN ]; then
-	MACADDR=`cat /sys/class/net/$WLAN/address`
+	WLANMAC=`cat /sys/class/net/$WLAN/address`
+else
+	WLANMAC="1"
 fi
-if [ -e /sys/class/net/br-lan ]; then
-	BRLANMAC=`cat /sys/class/net/br-lan/address`
+if [ -e /sys/class/net/$LAN ]; then
+	LANMAC=`cat /sys/class/net/$LAN/address`
 else 
-	BRLANMAC="0"
+	LANMAC="0"
 fi
 
 # Check if there is a MAC addr conflict and change if reqd.
-if [ $MACADDR = $BRLANMAC ]; then
+if [ $WLANMAC = $LANMAC ]; then
 	# Split up the MAC address
-	MAC1=`cat /sys/class/net/$WLAN/address | cut -c1-7`
-	MAC2=`cat /sys/class/net/$WLAN/address | cut -c8`
-	MAC3=`cat /sys/class/net/$WLAN/address | cut -c9-17`
+	MAC1=`cat /sys/class/net/$LAN/address | cut -c1-7`
+	MAC2=`cat /sys/class/net/$LAN/address | cut -c8`
+	MAC3=`cat /sys/class/net/$LAN/address | cut -c9-17`
 	# Change the third octet
 	if [ $MAC2 = "0" ]; then 
 		MAC2="1"
 	else
 		MAC2="0"
 	fi
+
 	# Set the new MAC for the Eth port and bridge.
-	ifconfig br-lan hw ether $MAC1$MAC2$MAC3
-	ifconfig eth0.1 hw ether $MAC1$MAC2$MAC3
+	ifconfig $LAN    down
+	ifconfig $LAN    hw ether $MAC1$MAC2$MAC3
+	ifconfig $BRIDGE hw ether $MAC1$MAC2$MAC3
+	ifconfig $LAN    up
 fi
 
 exit
--- SECN-build/files/etc/init.d/set-mac.sh
+++ SECN-build/files/etc/init.d/set-mac.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
 # /etc/init.d/set-mac
-# This script looks for a MAC address conflict between the mesh wifi interface and br-lan.
-# If both have the same MAC, batman-adv logs warning messages in dmesg re receiving packets on own address.
-# If there is a conflict, the MAC address of the AdHoc interface is changed in the third octet,
-# and is explictly set using the ifconfig command before the interface is added to br-lan.
-# This avoids batman-adv receiving packets on own MAC address
-
+# This script looks for a MAC address conflict between Mesh wifi interface and Ethernet LAN.
+# If there is a conflict, the MAC address of the LAN interface is changed in the third octet.
+# This avoids batman-adv receiving packets on own MAC address.
 
 WLAN="wlan0-1"
+LAN="eth0"
+BRIDGE="br-lan"
 
 # Get the current MAC addresses
 if [ -e /sys/class/net/$WLAN ]; then
-	MACADDR=`cat /sys/class/net/$WLAN/address`
+	WLANMAC=`cat /sys/class/net/$WLAN/address`
+else
+	WLANMAC="1"
 fi
-if [ -e /sys/class/net/br-lan ]; then
-	BRLANMAC=`cat /sys/class/net/br-lan/address`
+if [ -e /sys/class/net/$LAN ]; then
+	LANMAC=`cat /sys/class/net/$LAN/address`
 else 
-	BRLANMAC="0"
+	LANMAC="0"
 fi
 
 # Check if there is a MAC addr conflict and change if reqd.
-if [ $MACADDR = $BRLANMAC ]; then
+if [ $WLANMAC = $LANMAC ]; then
 	# Split up the MAC address
-	MAC1=`cat /sys/class/net/$WLAN/address | cut -c1-7`
-	MAC2=`cat /sys/class/net/$WLAN/address | cut -c8`
-	MAC3=`cat /sys/class/net/$WLAN/address | cut -c9-17`
+	MAC1=`cat /sys/class/net/$LAN/address | cut -c1-7`
+	MAC2=`cat /sys/class/net/$LAN/address | cut -c8`
+	MAC3=`cat /sys/class/net/$LAN/address | cut -c9-17`
 	# Change the third octet
 	if [ $MAC2 = "0" ]; then 
 		MAC2="1"
@@ -33,9 +34,11 @@ if [ $MACADDR = $BRLANMAC ]; then
 		MAC2="0"
 	fi
 
-	# Set the new MAC address for the mesh interface
-	ifconfig $WLAN hw ether $MAC1$MAC2$MAC3
-
+	# Set the new MAC for the Eth port and bridge.
+	ifconfig $LAN    down
+	ifconfig $LAN    hw ether $MAC1$MAC2$MAC3
+	ifconfig $BRIDGE hw ether $MAC1$MAC2$MAC3
+	ifconfig $LAN    up
 fi
 
 exit
