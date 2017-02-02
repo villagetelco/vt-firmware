d143b51ad47147e5c7de22ab1568f2e9f5501fab
tgillett <tgillett@gmail.com>
2017-02-02 21:51:29 +1000

    Update RACHEL memory set up for single USB devices

--- RACHEL-build/files/etc/init.d/set-rachel.sh
+++ RACHEL-build/files/etc/init.d/set-rachel.sh
@@ -1,61 +1,58 @@
 #! /bin/sh
 
 # Wait for memory devices to be available
-sleep 10
+sleep 15
 
-# ------------------------------
-
-# Mount the RACHEL memory device in case auto mount did not work.
 # Make dirs in case it is first boot
 mkdir /mnt/sda1
-mkdir /mnt/sda2
+mkdir /mnt/sdb1
+mkdir /mnt/sdc1
 
-mount -rw /dev/sda1  /mnt/sda1
-mount -rw /dev/sda2  /mnt/sda2
+# Unmount memory
+umount /mnt/sda1
+umount /mnt/sdb1
+umount /mnt/sdc1
 
-# Remove old links
-rm /www/rachel/modules
-rm /www/rachel/local
-rm /www/rachel/logs
+# Remove old links                                              
+rm /www/rachel
+rm /www/rachel_b
+rm /www/rachel_c
 
-# Set up default home page
-ln -s -f /www/rachel/rachel.index.html   /www/rachel/index.html
-
-# Find modules etc directories and force link
-
-# Check for VT-RACHEL SD/USB
-if [ -e "/mnt/sda1/modules" ]; then
-	ln -s -f /mnt/sda1/modules /www/rachel/modules
-	ln -s -f /mnt/sda1/art   /www/rachel/art
-	ln -s -f /mnt/sda1/css   /www/rachel/css
-	ln -s -f /mnt/sda1/local   /www/rachel/local
-	ln -s -f /mnt/sda1/index.html   /www/rachel/index.html # Set up VT-RACHEL home page
-	mkdir /mnt/sda1/logs
-	ln -s -f /mnt/sda1/logs    /www/rachel/logs
+# Mount memories and link
+if [ -e /dev/sda1 ]; then
+	mount -rw /dev/sda1  /mnt/sda1
+	ln -s -f /mnt/sda1   /www/rachel
+else # No memory present, point to dummy page
+	ln -s -f /www/rachel-x /www/rachel
+	ln -s -f /www/rachel/rachel.index.html   /www/rachel/index.html
 fi
 
-# Check for second VT-RACHEL SD/USB
-if [ -e "/mnt/sdb1/modules" ]; then
-	ln -s -f /mnt/sdb1/modules /www/rachel/modules
-	ln -s -f /mnt/sdb1/local   /www/rachel/local
-	ln -s -f /mnt/sdb1/index.html   /www/rachel/index.html # Set up VT-RACHEL home page
-	mkdir /mnt/sdb1/logs
-	ln -s -f /mnt/sdb1/logs    /www/rachel/logs
+if [ -e /dev/sdb1 ]; then
+	mount -rw /dev/sdb1  /mnt/sdb1
+	ln -s -f /mnt/sdb1   /www/rachel_b
 fi
 
-# Make sure cache directory exists
-if [ ! -d /mnt/sda1/cache ]; then 
-        mkdir /mnt/sda1/cache    
-fi 
+if [ -e /dev/sdc1 ]; then
+	mount -rw /dev/sdc1  /mnt/sdc1
+	ln -s -f /mnt/sdc1   /www/rachel_c
+fi
+
+# Set up cache
+if [ -e /dev/sda1 ]; then
+	mkdir /mnt/sda1/cache 
+	uci set polipo.cache.diskCacheRoot="/mnt/sda1/cache"
+	uci commit polipo
+else
+	uci set polipo.cache.diskCacheRoot=""
+	uci commit polipo
+fi
 
 # Set up logs
-mv /www/rachel/logs/log4.txt  /www/rachel/logs/log5.txt
-mv /www/rachel/logs/log3.txt  /www/rachel/logs/log4.txt
 mv /www/rachel/logs/log2.txt  /www/rachel/logs/log3.txt
 mv /www/rachel/logs/log1.txt  /www/rachel/logs/log2.txt
 mv /www/rachel/logs/log.txt   /www/rachel/logs/log1.txt
 
 echo "Start session" > /www/rachel/logs/log.txt
-
+date >> /www/rachel/logs/log.txt
 # -----------------------------
 
