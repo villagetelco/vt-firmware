c29e0d25be050e614e12fc718081ba15c0d8468b
tgillett <tgillett@gmail.com>
2017-02-08 22:41:10 +1000

    Updated coverage script for dual radios

---
+++ Duo-build/files/etc/init.d/set_coverage.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# This script sets the wifi "coverage" 0-99
+# Each increment corresponds to 3uSec of air propogation time.
+
+# Check assignment of phy0, phy1
+WIFI_INT=`uci get wireless.radio0.phy`
+
+if [ $WIFI_INT = "phy0" ]; then	
+	COVERAGE=`uci get wireless.radio0.coverage`
+	iw phy phy0 set coverage $COVERAGE
+	COVERAGE=`uci get wireless.radio1.coverage`
+	iw phy phy1 set coverage $COVERAGE
+else
+	COVERAGE=`uci get wireless.radio0.coverage`
+	iw phy phy1 set coverage $COVERAGE
+	COVERAGE=`uci get wireless.radio1.coverage`
+	iw phy phy0 set coverage $COVERAGE
+fi
